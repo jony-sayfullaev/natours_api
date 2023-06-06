c22f907b5db4fa0293559c75e039530cd3fa35e4
Jonibek Sayfullaev <jony@Jonibeks-MacBook-Air.local>
2023-06-06 14:56:51 +0900

    feat: updating and deleting tours process

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 const express = require("express");
 const app = express();
 const fs = require("fs");
+const morgan = require("morgan");
+
 // dotenv
 const dotenv = require("dotenv");
 dotenv.config();
 
-// Route
-app.get("/", (req, res) => {
-  res.status(200).json({ status: "success" });
-});
+// MIDDLEWARES
+app.use(morgan("dev"));
+app.use(express.json());
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
-app.get("/api/v1/tours", (req, res) => {
+// ROUTE HANDLERS
+const getAllTours = (req, res) => {
   res.status(200).json({
     status: "success",
     results: tours.length,
@@ -22,13 +24,81 @@ app.get("/api/v1/tours", (req, res) => {
       tours,
     },
   });
+};
+const getTour = (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+
+  if (!tour) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid Id",
+    });
+  }
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      tour,
+    },
+  });
+};
+const createTour = (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+  fs.writeFile(`${__dirname}/de v-data/data/tours-simple.json`, (err) => {
+    res.status(201).json({
+      status: "success",
+      data: {
+        tour: newTour,
+      },
+    });
+  });
+};
+
+const updateTour = (req, res) => {
+  if (req.params.id * 1 < tours.length) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid Id",
+    });
+  }
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      tour: "<Updated tour here...>",
+    },
+  });
+};
+const deleteTour = (req, res) => {
+  if (req.params.id * 1 < tours.length) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid Id",
+    });
+  }
+
+  res.status(200).json({
+    status: "success",
+    data: null,
+  });
+};
+
+// ROUTES
+app.get("/", (req, res) => {
+  res.status(200).json({ status: "success" });
 });
 
-// app.post("/api/v1/tours", (req, res) => {
+app.route("/api/v1/tours/").get(getAllTours).post(createTour);
+app
+  .route("/api/v1/tours/:id")
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
 
-// });
-
-//
+// SERVER
 const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`App running on port http://localhost:${port}`);
